4e4af5c9d37771bf29b45b59a5b15626b06f3439
Andrea Dal Ponte <info@andreadalponte.com>
2016-02-06 20:07:12 +0100

    Fix: ensure rails is loaded

--- lib/amministrativa.rb
+++ lib/amministrativa.rb
@@ -1,17 +1,18 @@
-require "amministrativa/engine"
+require 'rails'
+require 'amministrativa/engine'
 
 module Amministrativa
   # The parent controller all Amministrativa controllers inherits from.
   # Defaults to ApplicationController. This should be set early
   # in the initialization process and should be set to a string.
   mattr_accessor :parent_controller
-  @@parent_controller = 'ActionController::Base'
+  @@parent_controller = 'ActionController::Base'.freeze
 
   mattr_accessor :responder_class
-  @@responder_class = 'Amministrativa::Responder'
+  @@responder_class = 'Amministrativa::Responder'.freeze
 
   mattr_accessor :responder_formats
-  @@responder_formats = [:html, :json]
+  @@responder_formats = [:html, :json].freeze
 
   # Default way to setup Devise. Run rails generate devise_install to create
   # a fresh initializer with all configuration values.
