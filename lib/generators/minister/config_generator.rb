684e029d81c08ee16a65429c9307e61cefc1702d
Andrea Dal Ponte <info@andreadalponte.com>
2016-02-14 17:27:42 +0100

    Change: better naming for the config initializer

--- lib/generators/minister/config_generator.rb
+++ lib/generators/minister/config_generator.rb
@@ -3,12 +3,12 @@ require 'rails/generators/base'
 module Minister
   module Generators
     # Generate an minister initializer to override default configurations
-    class InstallGenerator < Rails::Generators::Base
+    class ConfigGenerator < Rails::Generators::Base
       source_root File.expand_path('../../templates', __FILE__)
 
       desc 'Creates an Minister initializer to your application.'
       def copy_initializer
-        template 'minister.rb', 'config/initializers/minister.rb'
+        template 'initializer.rb', 'config/initializers/minister.rb'
       end
     end
   end
---
+++ lib/generators/templates/initializer.rb
@@ -0,0 +1,7 @@
+Minister.setup do |config|
+  # config.parent_controller = 'ActionController::Base'
+
+  # config.responder_class = 'Minister::Responder'
+
+  # config.responder_formats = [:html, :json]
+end
