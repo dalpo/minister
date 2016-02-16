ed8c514645798dfb78dbc9e1ee567c78e78783f9
Andrea Dal Ponte <info@andreadalponte.com>
2016-02-16 19:40:37 +0100

    Change: better config generator struct

--- lib/generators/minister/config/config_generator.rb
+++ lib/generators/minister/config/config_generator.rb
@@ -4,9 +4,9 @@ module Minister
   module Generators
     # Generate an minister initializer to override default configurations
     class ConfigGenerator < Rails::Generators::Base
-      source_root File.expand_path('../../templates', __FILE__)
+      source_root File.expand_path('../templates', __FILE__)
 
-      desc 'Creates an Minister initializer to your application.'
+      desc 'Creates a Minister initializer to your application.'
       def copy_initializer
         template 'initializer.rb', 'config/initializers/minister.rb'
       end
